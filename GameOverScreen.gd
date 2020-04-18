5b2f9271b78f4399a95ccb203a2d4ba39b64f106
strawberriesandcheese <ulrike.herwig@student.hpi.de>
2020-04-18 19:02:41 +0200

    Update GameOverScreen.gd
    Co-Authored-By: Julian Berger <derjulian.berger@web.de>

--- GameOverScreen.gd
+++ GameOverScreen.gd
@@ -4,9 +4,6 @@ signal QuitButton_pressed()
 signal RestartButton_pressed()
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
