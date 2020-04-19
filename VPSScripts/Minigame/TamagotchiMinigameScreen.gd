32f7d04fa84e8f2f390565ae5ef98e0d519c5ed2
Julian Berger <julian.berger@student.hpi.de>
2020-04-19 11:40:52 +0200

    Add lives and bullets

--- VPSScripts/Minigame/TamagotchiMinigameScreen.gd
+++ VPSScripts/Minigame/TamagotchiMinigameScreen.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var timePassed : float = 5
 var enemyScene = preload("res://VPSScenes/Minigame/MinigameEnemy.tscn")
+var bulletScene = preload("res://VPSScenes/Minigame/Bullet.tscn")
 var paused: bool = false
 
 # Called when the node enters the scene tree for the first time.
@@ -29,10 +30,21 @@ func moveRight():
 	$Player.position.x += 10
 
 func shoot():
-	pass
+	var bullet = bulletScene.instance()
+	bullet.position = $Player.position
+	add_child(bullet)
 
 func pause():
 	paused = true
 
 func unpause():
 	paused = false
+
+
+func _on_Player_area_entered(area):
+	for heart in $HeartContainer.get_children():
+		if heart.visible:
+			heart.visible = false
+			return
+			
+	print("You died")
