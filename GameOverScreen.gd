565a8c1b46ecf703e5a92e7967fbc72fcadf5fbf
strawberriesandcheese <ulrike.herwig@student.hpi.de>
2020-04-20 17:50:58 +0200

    make game over screen more beautiful

--- GameOverScreen.gd
+++ GameOverScreen.gd
@@ -10,9 +10,19 @@ func _ready():
 
 func popup():
 	visible = true
+	$Timer.start()
+
+func show_buttons():
+	$ButtonContainer.visible = true
 
 func _on_QuitButton_pressed():
 	emit_signal("QuitButton_pressed")
 
 func _on_RestartButton_pressed():
 	emit_signal("RestartButton_pressed")
+
+func _on_Timer_timeout():
+	show_buttons()
+
+func _on_GameOverScreen_gui_input(event):
+	show_buttons()
