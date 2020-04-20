6fe3e01a9d17fd58df6ae0c1e6891137712dd475
strawberriesandcheese <ulrike.herwig@student.hpi.de>
2020-04-21 01:52:24 +0200

    rework dialogue

--- VNScripts/CharacterView.gd
+++ VNScripts/CharacterView.gd
@@ -24,6 +24,7 @@ onready var positions = [
 ]
 
 onready var backgroundMap = {
+	"black": $Black,
 	"city": $BackgroundCity,
 	"class": $BackgroundClass,
 	"deep sea": $BackgroundDeepSea,
@@ -47,6 +48,7 @@ func add_characters(listOfCharacters):
 	listOfCharacters.erase("lucas young")
 	listOfCharacters.erase("lucas")
 	listOfCharacters.erase("lucas old")
+	listOfCharacters.erase("lucas adult")
 	
 	for index in range(listOfCharacters.size()):
 		var character = add_character(listOfCharacters[index], positions[listOfCharacters.size() - 1][index])
