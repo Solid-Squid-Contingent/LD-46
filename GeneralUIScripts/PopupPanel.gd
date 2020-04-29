c5c9abc952cbb32b180fa29ed05481892f20a761
strawberriesandcheese <ulrike.herwig@student.hpi.de>
2020-04-29 21:42:18 +0200

    add better ui without functionality

---
+++ GeneralUIScripts/PopupPanel.gd
@@ -0,0 +1,46 @@
+extends PopupPanel
+
+signal ok_button_pressed()
+signal abort_button_pressed()
+
+onready var titleLabel = $VBoxContainer/Label
+onready var textLabel = $VBoxContainer/RichTextLabel
+onready var okButton = $VBoxContainer/HBoxContainer/OkButton
+onready var abortButton = $VBoxContainer/HBoxContainer/AbortButton
+onready var timer = $Timer
+
+func _ready():
+	abortButton.visible = false
+	
+func set_title(title: String):
+	titleLabel.text = title
+
+func set_text(text: String):
+	textLabel.text = text
+
+func hide_after_sec(seconds):
+	timer.start(seconds)
+
+func hide_ok_button():
+	okButton.visible = false
+
+func hide_abort_button():
+	abortButton.visible = false
+
+func show_ok_button():
+	okButton.visible = true
+
+func show_abort_button():
+	abortButton.visible = true
+
+func hide():
+	self.visible = false
+
+func _on_Timer_timeout():
+	hide()
+
+func _on_OkButton_pressed():
+	emit_signal("ok_button_pressed")
+
+func _on_AbortButton_pressed():
+	emit_signal("abort_button_pressed")
--- GeneralUIScripts/SavingScreen.gd
+++ GeneralUIScripts/SavingScreen.gd
@@ -7,6 +7,9 @@ signal button_pressed()
 onready var savesList = $VBoxContainer/ListContainer/ItemList
 const Util = preload("res://SaveFileUtils.gd")
 
+onready var popupPanel = $PopupPanel
+onready var lineEdit = $VBoxContainer/MarginContainer/LineEdit
+
 func _ready():
 	visible = false
 
@@ -17,18 +20,51 @@ func popup():
 func go_away():
 	visible = false
 
+func popup_saved_game():
+	popupPanel.set_title("Saved!")
+	popupPanel.set_text("Your current progress was saved. You can now savely quit the game without losing your progress.")
+	popupPanel.hide_ok_button()
+	popupPanel.hide_abort_button()
+	popupPanel.hide_after_sec(3)
+	
+func popup_overwrite_warning():
+	popupPanel.set_title("Overwrite?")
+	popupPanel.set_text("If you proceed you'll overwrite this save. Are you really sure?")
+	popupPanel.show_ok_button()
+	popupPanel.show_abort_button()
+	popupPanel.connect("ok_button_pressed", self, "ok_overwrite")
+	popupPanel.connect("abort_button_pressed", self, "abort_overwrite")
+
+func ok_overwrite():
+	#TODO: actually save
+	popupPanel.disconnect("ok_button_pressed", self, "ok_overwrite")
+	popupPanel.disconnect("abort_button_pressed", self, "abort_overwrite")
+
+func abort_overwrite():
+	popupPanel.hide()
+	popupPanel.disconnect("abort_button_pressed", self, "abort_overwrite")
+	popupPanel.disconnect("ok_button_pressed", self, "ok_overwrite")
+
+func save():
+	if not lineEdit.text.is_valid_filename():
+		return #TODO: Error Message
+	emit_signal("save", lineEdit.text)
+
+func change_save_name(newName):
+	lineEdit.text = newName
 
 func _on_BackButton_pressed():
 	emit_signal("backButton_pressed")
 	emit_signal("button_pressed")
 
-func _on_LineEdit_text_entered(new_text):
-	if not new_text.is_valid_filename():
-		return #TODO: Error message
-
-	emit_signal("save", new_text)
+# warning-ignore:unused_argument
+func _on_LineEdit_text_entered(newText):
+	save()
 	emit_signal("button_pressed")
 
-
 func _on_ItemList_item_selected(index):
-	savesList.unselect_all()
+	lineEdit.text = $VBoxContainer/ListContainer/ItemList.get_item_text(index)
+
+func _on_SaveButton_pressed():
+	save()
+	emit_signal("button_pressed")
